1068824a7d53725e6ea10cf7879a41c82a02e06f
rizwanullah313 <ru323091@gmail.com>
2021-09-22 14:03:34 +0500

    using alert in react

--- src/home.js
+++ src/home.js
@@ -1,25 +1,38 @@
 import React, { useState } from "react";
+import Alert from "./components/Alert";
 // import abc from "./file1";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 // import About from "./components/About";
 
 function Home() {
-  const [mode, setMode] = useState("light");
-  // let name = "bahria";
-  const toggleMode = () => {
-    if (mode === "light") {
-      setMode("dark");
-      document.body.style.backgroundColor = "grey";
-    } else {
-      setMode("light");
-      document.body.style.backgroundColor = "black";
-    }
-  };
+    const [mode, setMode] = useState("light");
+    // let name = "bahria";
+    const toggleMode = () => {
+        if (mode === "light") {
+            setMode("dark");
+            document.body.style.backgroundColor = "grey";
+            showAlert(" Dark Mode has been Enabled", "primary " );
+        } else {
+            setMode("light");
+            document.body.style.backgroundColor = "gray";
+            showAlert(" White Mode has been Enabled", "primary ")
 
-  return (
-    <div>
-      {/* <nav>
+        }
+    };
+
+    const [alert, setAlert] = useState(null); // alert ka object
+
+    const showAlert = (message, type) => {
+        setAlert({
+            msg: message,
+            type: type
+        })
+    }
+
+    return (
+        <div>
+            {/* <nav>
         <li>Home</li>
         <li>About</li>
         <li>Contact</li>
@@ -32,20 +45,21 @@ function Home() {
         check the Laravel version. The above output shows that you are running
         Laravel Framework 7.17.
       </p> */}
-      <Navbar
-        tname="bahria"
-        abouttxt="About Us"
-        mode={mode}
-        toggleMode={toggleMode}
-      />
-      {/*    <Navbar/> */}
-      <br />
-      <hr /> <hr />
-      <div className="container my-3">
-        <TextForm heading="Enter the text to analyze" mode={mode} />
-        {/* <About /> */}
-      </div>
-    </div>
-  );
+            <Navbar
+                tname="bahria"
+                abouttxt="About Us"
+                mode={mode}
+                toggleMode={toggleMode}
+            />
+            {/*    <Navbar/> */}
+            <br />
+            <hr /> <hr />
+            <div className="container my-3">
+                <TextForm heading="Enter the text to analyze" mode={mode} />
+                {/* <About /> */}
+                <Alert alert={alert} />
+            </div>
+        </div>
+    );
 }
 export default Home;
