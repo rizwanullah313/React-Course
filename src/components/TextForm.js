4e0abb762585d6b11ba869e79e89c96854569cfd
rizwanullah313 <ru323091@gmail.com>
2021-09-23 13:05:25 +0500

    fixing words counter issue

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -104,7 +104,7 @@ export default function TextForm(props) {
       >
         <h1>Your Text Summary</h1>
         <p>
-          {text.split(" ").filter((element)=>{
+          {text.split(/\s+/).filter((element)=>{
             return element.length!==0}).length} words, {text.length} characters
         </p>
         <p>{0.008 * text.split(" ").filter((element)=>{
